45ccefde862535d152c5573ed1f39b4d07a0ee47
Miguel Botelho <135281223+Bot-e-lho@users.noreply.github.com>
2023-09-23 18:56:16 -0300

    Update Tabuleiro.java

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -131,27 +131,29 @@ public final class Tabuleiro extends JFrame {
         });
         
         coletar.addActionListener(e -> {
-            int posXJogador = jogador.getPosicao().getX();
-            int posYJogador = jogador.getPosicao().getY();
-    
-            if (posXJogador >= 0 && posXJogador < tamanhoTabuleiro && posYJogador >= 0 && posYJogador < tamanhoTabuleiro) {
-                int elemento = ambiente.getElemento(jogador.getPosicao());
-                switch (elemento) {
-                    case Ambiente.ouro -> {
-                        ambiente.setConteudoPosicao(posXJogador, posYJogador, Ambiente.vazio);
-                    }
-                    case Ambiente.madeira -> {
-                        ambiente.setConteudoPosicao(posXJogador, posYJogador, Ambiente.vazio);
-                    }
-                    case Ambiente.poco -> {
-                        jogador.diminuirEnergiaVital(100);
-                    }
-                    default -> {
-                    }
+        int posXJogador = jogador.getPosicao().getX();
+        int posYJogador = jogador.getPosicao().getY();
+
+        if (posXJogador >= 0 && posXJogador < tamanhoTabuleiro && posYJogador >= 0 && posYJogador < tamanhoTabuleiro) {
+            String elemento = ambiente.getElemento(jogador.getPosicao());
+            switch (elemento) {
+                case Ambiente.ouro -> {
+                    ambiente.setConteudoPosicao(posXJogador, posYJogador, Ambiente.vazio);
+                 }
+                case Ambiente.madeira -> {
+                    ambiente.setConteudoPosicao(posXJogador, posYJogador, Ambiente.vazio);
                 }
-                atualizarTabuleiro();
+                case Ambiente.poco -> {
+                    jogador.diminuirEnergiaVital(100);
+                }
+                default -> {                
+                abrirJanela("Nada para coletar");
+                }
             }
+            atualizarTabuleiro();
+        }
         });
+
 
 
         botoesFrame.add(botoesPanel, BorderLayout.CENTER);
